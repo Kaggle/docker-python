ae7a2bc7cb685c4a81248b35075460eb477e567a
Aurelio Agundez <aurelio@agundez.com>
2017-12-19 08:54:29 -0800

    Fetch proxy project from env var

--- patches/sitecustomize.py
+++ patches/sitecustomize.py
@@ -1,18 +1,18 @@
 # Monkey patches BigQuery client creation to use proxy.
 import os
 
-kaggle_proxy_data = os.getenv("KAGGLE_PROXY_DATA")
-if kaggle_proxy_data:
+kaggle_proxy_token = os.getenv("KAGGLE_PROXY_TOKEN")
+if kaggle_proxy_token:
     from google.auth import credentials
     from google.cloud import bigquery
     from google.cloud.bigquery._http import Connection
 
     Connection.API_BASE_URL = os.getenv("KAGGLE_PROXY_URL")
-    Connection._EXTRA_HEADERS["X-KAGGLE-PROXY-DATA"] = kaggle_proxy_data
+    Connection._EXTRA_HEADERS["X-KAGGLE-PROXY-DATA"] = kaggle_proxy_token
 
     bq_client = bigquery.Client
     bigquery.Client = lambda *args, **kwargs: bq_client(
         *args,
         credentials=credentials.AnonymousCredentials(),
-        project="bigquery-public-data",
+        project=os.getenv("KAGGLE_PROXY_PROJECT"),
         **kwargs)
