c79fa24aa8a183e24794325ea7f194f54b5d60c9
Aurelio Agundez <aurelio@agundez.com>
2018-01-17 08:51:36 -0800

    Suppress exception in call to `AnonymousCredentials.refresh` with NOP

--- patches/sitecustomize.py
+++ patches/sitecustomize.py
@@ -1,18 +1,20 @@
 # Monkey patches BigQuery client creation to use proxy.
 import os
 
-kaggle_proxy_token = os.getenv("KAGGLE_PROXY_TOKEN")
+kaggle_proxy_token = os.getenv("KAGGLE_DATA_PROXY_TOKEN")
 if kaggle_proxy_token:
     from google.auth import credentials
     from google.cloud import bigquery
     from google.cloud.bigquery._http import Connection
 
-    Connection.API_BASE_URL = os.getenv("KAGGLE_PROXY_URL")
+    Connection.API_BASE_URL = os.getenv("KAGGLE_DATA_PROXY_URL")
     Connection._EXTRA_HEADERS["X-KAGGLE-PROXY-DATA"] = kaggle_proxy_token
 
     bq_client = bigquery.Client
     bigquery.Client = lambda *args, **kwargs: bq_client(
         *args,
         credentials=credentials.AnonymousCredentials(),
-        project=os.getenv("KAGGLE_PROXY_PROJECT"),
+        project=os.getenv("KAGGLE_DATA_PROXY_PROJECT"),
         **kwargs)
+
+    credentials.AnonymousCredentials.refresh = lambda *args: None
