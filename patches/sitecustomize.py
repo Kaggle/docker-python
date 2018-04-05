97894a171d1831838da28b757aabb352bc5ecfd9
Seb Boving <seb@google.com>
2018-04-05 00:36:06 +0000

    Revert "Support adding an /etc/host entry for the data proxy, if asked."
    This reverts commit 062f975d92c5795feb530e3ea1914d3c7dd3a96b.
    
    There is no more need for this support in the docker image.  It is fully
    externally handled through docker run's `--add-host` feature.

--- patches/sitecustomize.py
+++ patches/sitecustomize.py
@@ -5,26 +5,11 @@
 import torch
 import os
 
-_HOST_FILE = "/etc/hosts"
-
 kaggle_proxy_token = os.getenv("KAGGLE_DATA_PROXY_TOKEN")
 if kaggle_proxy_token:
     from google.auth import credentials
     from google.cloud import bigquery
     from google.cloud.bigquery._http import Connection
-
-    host_entry = os.getenv("KAGGLE_DATA_PROXY_HOST_ENTRY")
-    if host_entry:
-        host_entry = host_entry.strip()
-        with open(_HOST_FILE, "r") as host_file:
-            for line in host_file:
-                if line.strip() == host_entry:
-                    break
-            else:
-                with open(_HOST_FILE, "a") as host_file_append:
-                    host_file_append.write("\n"  # Just in case it wasn't newline terminated.
-                                           + host_entry
-                                           + "\n")
 
     Connection.API_BASE_URL = os.getenv("KAGGLE_DATA_PROXY_URL")
     Connection._EXTRA_HEADERS["X-KAGGLE-PROXY-DATA"] = kaggle_proxy_token
