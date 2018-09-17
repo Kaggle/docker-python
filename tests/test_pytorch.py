560b621823923f961e3ef45a8a2ea1c6de5306ce
Vincent Roseberry <rosbo@google.com>
2018-09-17 16:03:21 +0000

    Merge and improve gpu tests. Add annotations

--- tests/test_pytorch.py
+++ tests/test_pytorch.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as tnn
 import torch.autograd as autograd
 
+from common import gpu_test
+
+
 class TestPyTorch(unittest.TestCase):
     # PyTorch smoke test based on http://pytorch.org/tutorials/beginner/nlp/deep_learning_tutorial.html
     def test_nn(self):
@@ -11,3 +14,12 @@ class TestPyTorch(unittest.TestCase):
         linear_torch = tnn.Linear(5,3)
         data_torch = autograd.Variable(torch.randn(2, 5))
         linear_torch(data_torch)
+
+    @gpu_test
+    def test_gpu(self):
+        cuda = torch.device('cuda')  
+        a = torch.tensor([1., 2.], device=cuda)
+
+        result = a.sum()
+
+        self.assertEqual(torch.tensor([3.], device=cuda), result)
