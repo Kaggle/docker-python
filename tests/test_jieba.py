560af468065cfd51c3b96d6526a19fe5d2817715
Gyczero <2997347185@qq.com>
2018-10-02 12:18:13 +0800

    Add an assertion for jieba

--- tests/test_jieba.py
+++ tests/test_jieba.py
@@ -8,3 +8,6 @@ class TestJieba(unittest.TestCase):
     def test_text_split(self):
         sentence = "我爱北京天安门"
         seg_list = jieba.cut(sentence)
+        seg_list = list(seg_list)
+
+        self.assertEqual(4, len(seg_list))
