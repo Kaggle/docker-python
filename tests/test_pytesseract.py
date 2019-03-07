e4024b9224b5d5a472ac33dca9e9931921330fd1
Vincent Roseberry <rosbo@google.com>
2019-03-07 21:51:14 +0000

    Fix tesseract unit test

--- tests/test_pytesseract.py
+++ tests/test_pytesseract.py
@@ -2,6 +2,7 @@ import unittest
 import io
 import pytesseract
 import numpy as np
+from PIL import Image
 from wand.image import Image as wandimage
 
 class TestPytesseract(unittest.TestCase):
@@ -10,5 +11,5 @@ class TestPytesseract(unittest.TestCase):
         with wandimage(filename='/input/tests/data/test.pdf') as wand_image:
             img_buffer = np.asarray(bytearray(wand_image.make_blob(format='png')), dtype='uint8')
             bytesio = io.BytesIO(img_buffer)
-            test_string = pytesseract.image_to_string(PILImage.open(bytesio))
+            test_string = pytesseract.image_to_string(Image.open(bytesio))
             self.assertTrue(type(test_string) == str)
