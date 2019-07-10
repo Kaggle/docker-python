8b60802f5fcdfdd30ab2e773949320391e974e7a
Ilya Figotin <ifigotin@google.com>
2019-07-10 11:44:29 -0700

    Add a unit test for default creds

--- tests/test_gcs.py
+++ tests/test_gcs.py
@@ -1,12 +1,14 @@
 import unittest
 
-import mock
+from unittest.mock import Mock
 
+from kaggle_gcp import KaggleKernelCredentials
+from test.support import EnvironmentVarGuard
 from google.cloud import storage
 
 def _make_credentials():
     import google.auth.credentials
-    return mock.Mock(spec=google.auth.credentials.Credentials)
+    return Mock(spec=google.auth.credentials.Credentials)
 
 class TestStorage(unittest.TestCase):
 
@@ -21,3 +23,13 @@ class TestStorage(unittest.TestCase):
     def test_annonymous_client(self):
         anonymous = storage.Client.create_anonymous_client()
         self.assertIsNotNone(anonymous)
+
+    def test_deftault_credentials_gcs_enabled(self):
+        env = EnvironmentVarGuard()
+        env.set('KAGGLE_USER_SECRETS_TOKEN', 'foobar')
+        env.set('KAGGLE_KERNEL_INTEGRATIONS', 'GCS')
+        with env:
+            from sitecustomize import init
+            init()
+            client = storage.Client(project="xyz")
+            self.assertIsInstance(client._credentials, KaggleKernelCredentials)
