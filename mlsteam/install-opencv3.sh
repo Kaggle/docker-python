30e7afca25c7a61e3a133bd4c59be7fecd1a6c39
SimonChuang <simon.s.chuang@gmail.com>
2019-07-26 11:59:28 +0800

    Update packages include opencv with ffmpeg

--- mlsteam/install-opencv3.sh
+++ mlsteam/install-opencv3.sh
@@ -22,6 +22,9 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D WITH_TBB=ON \
       -D WITH_V4L=ON \
       -D WITH_OPENGL=ON \
+      -D WITH_FFMPEG=ON \
+      -D WITH_QT=OFF \
+      -D WITH_GTK=OFF \
       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
       -D BUILD_EXAMPLES=ON ..
 # make in parallel
