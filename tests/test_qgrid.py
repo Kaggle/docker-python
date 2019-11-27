eaf0f734493d83b64860db98808f38660718ea0a
Vincent Roseberry <rosbo@google.com>
2019-11-27 22:01:32 +0000

    fix qgrid test

--- tests/test_qgrid.py
+++ tests/test_qgrid.py
@@ -1,15 +1,16 @@
 import unittest
+
+import numpy as np
 import pandas as pd
+
 from qgrid import QgridWidget
 
+
 class TestQgrid(unittest.TestCase):
-    def test_nans():
+    def test_nans(self):
         df = pd.DataFrame([(pd.Timestamp('2017-02-02'), np.nan),
                            (4, 2),
                            ('foo', 'bar')])
         view = QgridWidget(df=df)
-        view._handle_qgrid_msg_helper({
-            'type': 'change_sort',
-            'sort_field': 1,
-            'sort_ascending': True
-        })
+        
+        self.assertIsNotNone(view.get_changed_df())
