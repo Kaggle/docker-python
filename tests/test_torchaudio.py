218a96adf36056ff170ff609dc492dc2f28ba5f8
Vincent Roseberry <rosbo@google.com>
2019-11-29 21:37:06 +0000

    Use assertEqual(...)

--- tests/test_torchaudio.py
+++ tests/test_torchaudio.py
@@ -7,5 +7,5 @@ class TestTorchaudio(unittest.TestCase):
     def test_import(self):
         waveform, sample_rate = torchaudio.load('/input/tests/data/test.wav')
 
-        self.assertEquals(2, len(waveform))
-        self.assertEquals(44100, sample_rate)
+        self.assertEqual(2, len(waveform))
+        self.assertEqual(44100, sample_rate)
