68f76bf9bd6e4d7ec31dc95caf5b31801a3ac5bb
Vincent Roseberry <rosbo@google.com>
2020-02-08 01:20:05 +0000

    Use semaphore to control access to GPU in tests

--- tests/common.py
+++ tests/common.py
@@ -3,4 +3,9 @@
 import os
 import unittest
 
+from threading import BoundedSemaphore
+
 gpu_test = unittest.skipIf(len(os.environ.get('CUDA_VERSION', '')) == 0, 'Not running GPU tests')
+
+# Tests using the GPU heavily must not be run concurrently.
+gpu_semaphore = BoundedSemaphore(1)
