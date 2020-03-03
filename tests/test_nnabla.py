a5b40fe31b9c502bfeb44e476a83cf3a294eb9af
takuseno <takuma.seno@gmail.com>
2020-03-03 14:45:56 +0900

    Add nnabla

---
+++ tests/test_nnabla.py
@@ -0,0 +1,28 @@
+import unittest
+
+import numpy as np
+import nnabla as nn
+import nnabla.functions as F
+from nnabla.ext_utils import get_extension_context
+
+from common import gpu_test
+
+
+class TestNNabla(unittest.TestCase):
+    def test_addition(self):
+        # entry variables
+        a = nn.Variable.from_numpy_array(np.random.random())
+        b = nn.Variable.from_numpy_array(np.random.random())
+
+        # add operation
+        c = a + b
+
+        # forward
+        c.forward()
+
+        self.assertAlmostEqual(c.d, a.d + b.d)
+
+    @gpu_test
+    def test_cuda_ext(self):
+        ctx = get_extension_context('cudnn', device_id='0')
+        nn.set_default_context(ctx)
