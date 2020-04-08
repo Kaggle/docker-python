4170807e4a1c70eef6416fe3f1661c9c1c99a9da
Vincent Roseberry <rosbo@google.com>
2020-04-08 21:24:04 +0000

    Add pysal test

---
+++ tests/test_pysal.py
@@ -0,0 +1,8 @@
+import unittest
+
+from pysal.weights import lat2W
+
+class TestPysal(unittest.TestCase):
+    def test_distance_band(self):
+        w = lat2W(4,4)
+        self.assertEqual(16, w.n)
