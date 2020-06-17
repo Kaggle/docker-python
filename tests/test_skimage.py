7609ecbce4bb0961f3ff7dfe6bc9b19a7cf1d19e
Vincent Roseberry <rosbo@google.com>
2020-06-17 02:29:28 +0000

    Add tif codec test.

--- tests/test_skimage.py
+++ tests/test_skimage.py
@@ -1,8 +1,13 @@
 import unittest
 
-from skimage import data, filters
+from skimage import data, filters, io
 
 class TestSkImage(unittest.TestCase):
     def test_filter(self):
         image = data.coins()
         filters.sobel(image)
+
+    def test_codecs(self):
+        img = io.MultiImage('/input/tests/data/test.tif')
+        
+        self.assertEqual((10, 10, 4), img[0].shape)
