25beb81c430590024a04b95e388c2775808ee100
Bovard Doerschuk-Tiberi <bovard@google.com>
2021-03-01 23:33:17 +0000

    Added more bq-tf tests

--- tests/test_tensorflow_bigquery.py
+++ tests/test_tensorflow_bigquery.py
@@ -9,6 +9,7 @@ class TestTensorflowBigQuery(unittest.TestCase):
     # Some versions of bigquery crashed tensorflow, add this test to make sure that doesn't happen.
     # python -c "from google.cloud import bigquery; import tensorflow". This flow is common because bigquery is imported in kaggle_gcp.py
     # which is loaded at startup.
-    def test_addition(self):        
+    def test_matrix_addition(self):
         result = tf.add([1, 2], [3, 4])
         self.assertEqual([2], result.shape)
+        self.assertListEqual([4, 6], [x for x in result.numpy()])
