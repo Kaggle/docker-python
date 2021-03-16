9cf2ac1a10f524e8b3426bfcb3fcaf5107f9d9f4
Philmod <philmod@google.com>
2021-03-16 12:42:22 -0400

    Fix path to image

--- tests/test_easyocr.py
+++ tests/test_easyocr.py
@@ -4,5 +4,5 @@ import easyocr
 class TestEasyOCR(unittest.TestCase):
     def test_readtext(self):
     	reader = easyocr.Reader(['ch_sim','en']) # need to run only once to load model into memory
-		result = reader.readtext('chinese.jpg', detail = 0)
+		result = reader.readtext('/input/tests/data/chinese.jpg', detail = 0)
         self.assertEqual(len(result), 8)
