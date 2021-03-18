3998a18ac6d1452b4a6476ebcec7e064b6a45c49
Philippe Modard <philmod@google.com>
2021-03-18 00:57:53 +0000

    Fix test to work with pre-downloaded models

--- tests/test_easyocr.py
+++ tests/test_easyocr.py
@@ -1,8 +1,15 @@
 import unittest
+
 import easyocr
 
 class TestEasyOCR(unittest.TestCase):
     def test_readtext(self):
-        reader = easyocr.Reader(['en'], gpu=False)  # need to run only once to load model into memory
+        # The model_storage_directory is only need in tests where we overwrite HOME=/tmp
+        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory='/root/.EasyOCR/model')
         result = reader.readtext('/input/tests/data/english.png', detail = 0)
-        self.assertEqual(len(result), 7)
+        # ['Reduce your risk of coronavirus infection:', 'Clean hands with soap and water', 
+        # 'or alcohol based hand rub', 'Cover nose and mouth when coughing and', 
+        # 'sneezing with tissue or flexed elbow', 'Avoid close contact with anyone with', 
+        # 'cold or flu like symptoms', 'Thoroughly cook meat and eggs', 
+        # 'No unprotected contact with live wild', 'or farm animals', 'World Health', 'Organization']
+        self.assertEqual(len(result), 12)
