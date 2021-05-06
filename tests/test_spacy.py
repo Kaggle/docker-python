7f983e2cca1863d41631edaffddea7c6fab986ad
Vincent Roseberry <rosbo@google.com>
2021-05-06 12:13:41 -0700

    Fix spacy test. (#1008)
    spacy has been upgraded to 3.x (fastai had a 2.x requirement but now
    support 3.x which upgraded spacy.)

--- tests/test_spacy.py
+++ tests/test_spacy.py
@@ -4,6 +4,6 @@ import spacy
 
 class TestSpacy(unittest.TestCase):
     def test_model(self):
-        nlp = spacy.load('en')
+        nlp = spacy.load('en_core_web_sm')
         doc = nlp('This is a sentence.')
         self.assertEqual(5, len(doc))
