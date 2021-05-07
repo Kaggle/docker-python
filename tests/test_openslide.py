c287f51ffa498f0a49ed65193169fdcb8da31893
Vincent Roseberry <rosbo@google.com>
2021-05-07 10:17:06 -0700

    Fix warning in openslide test (#1009)
    ```
    ResourceWarning: unclosed file <_io.BufferedReader name='/input/tests/data/test.tif'>
      testMethod()
    ResourceWarning: Enable tracemalloc to get the object allocation traceback
    ```

--- tests/test_openslide.py
+++ tests/test_openslide.py
@@ -4,6 +4,5 @@ from openslide import open_slide
 
 class TestOpenslide(unittest.TestCase):
     def test_read_tif(self):
-        slide = open_slide('/input/tests/data/test.tif')
-        
-        self.assertEqual(1, slide.level_count)
+        with open_slide('/input/tests/data/test.tif') as slide:
+            self.assertEqual(1, slide.level_count)
