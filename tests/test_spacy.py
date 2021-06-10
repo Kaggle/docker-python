3f0701e8c973646775f8fa25c888254f717b2b7b
Alexis Cook <alexis.cook@gmail.com>
2021-06-10 12:12:41 -0400

    Update test_spacy.py (#1026)

--- tests/test_spacy.py
+++ tests/test_spacy.py
@@ -2,6 +2,7 @@ import unittest
 
 import spacy
 
+# please check with learn team before changing
 class TestSpacy(unittest.TestCase):
     def test_model(self):
         nlp = spacy.load('en_core_web_sm')
