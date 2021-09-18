2ac2f65c2cc550ca8c206a9390a2f1617e3c6bdf
mariosasko <mariosasko777@gmail.com>
2021-09-18 19:03:48 +0200

    Use appropriate name for test function

--- tests/test_hf_datasets.py
+++ tests/test_hf_datasets.py
@@ -4,7 +4,7 @@ from datasets import Dataset
 
 
 class TestHfDatasets(unittest.TestCase):
-    def test_adam_w(self):
+    def test_create_dataset_from_dict(self):
         dset = Dataset.from_dict({"col1": ["a", "b", "c"], "col2": [1, 2, 3]})
         self.assertEqual(dset.num_rows, 3)
         self.assertEqual(dset.num_columns, 2)
