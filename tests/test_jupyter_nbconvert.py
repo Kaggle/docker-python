66a12acb6bb1e920b88f65331e5609136067fe02
Vincent Roseberry <rosbo@google.com>
2022-03-28 13:02:46 -0700

    Install latest version of nbconvert to fix Jinja issue. (#1141)
    The latest version of Jinja changed the import for `Markup`.
    
    The import was fixed in the latest version of `nbconvert`: https://github.com/jupyter/nbconvert/issues/1736
    
    We include the version of `nbconvert` from the DLVM base image which is
    released every ~2 weeks. However, the `nbconvert` fix was released only
    6 hours ago.
    
    Temporarily install thel latest version of `nbconvert`.
    
    Add a test to prevent regression.
    
    http://b/227194111

--- tests/test_jupyter_nbconvert.py
+++ tests/test_jupyter_nbconvert.py
@@ -3,12 +3,28 @@ import unittest
 import subprocess
 
 class TestJupyterNbconvert(unittest.TestCase):
-    def test_nbconvert(self):
+    def test_nbconvert_to_notebook(self):
         result = subprocess.run([
             'jupyter',
             'nbconvert',
             '--to',
             'notebook',
+            '--template',
+            '/opt/kaggle/nbconvert-extensions.tpl',
+            '--execute',
+            '--stdout',
+            '/input/tests/data/notebook.ipynb',
+        ], stdout=subprocess.PIPE)
+
+        self.assertEqual(0, result.returncode)
+        self.assertTrue(b'999' in result.stdout)
+
+    def test_nbconvert_to_html(self):
+        result = subprocess.run([
+            'jupyter',
+            'nbconvert',
+            '--to',
+            'html',
             '--template',
             '/opt/kaggle/nbconvert-extensions.tpl',
             '--execute',
