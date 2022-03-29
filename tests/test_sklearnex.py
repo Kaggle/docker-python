2fab7f612d5f2c1e787146bd9c6e469bb09a123a
Vincent Roseberry <rosbo@google.com>
2022-03-29 15:48:04 +0000

    update test

--- tests/test_sklearnex.py
+++ tests/test_sklearnex.py
@@ -2,12 +2,9 @@ import unittest
 
 import numpy as np
 
-from sklearn.cluster import DBSCAN
-
 class TestSklearnex(unittest.TestCase):
     def test_dbscan(self):
-        from sklearnex import patch_sklearn
-        patch_sklearn()
+        from sklearnex.cluster import DBSCAN
         X = np.array([[1., 2.], [2., 2.], [2., 3.],
             [8., 7.], [8., 8.], [25., 80.]], dtype=np.float32)
 
