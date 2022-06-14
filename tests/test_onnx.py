b1880331e27607487a5d68e3055f330a1a03503e
Philmod <philmod@google.com>
2022-06-14 16:46:42 -0400

    add an assertion

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -5,3 +5,4 @@ import onnx
 class TestOnnx(unittest.TestCase):
     def test_load(self):
         model = onnx.load("data/mnist-8.onnx")
+        self.assertIn("CNTKGraph", onnx.helper.printable_graph(model.graph))
