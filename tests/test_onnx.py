d1eaca856822b3f0364b7835ccd9a881e7f3b7e0
Philippe Modard <philmod@google.com>
2022-06-15 13:23:13 +0000

    fix path to model file

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -4,5 +4,5 @@ import onnx
 
 class TestOnnx(unittest.TestCase):
     def test_load(self):
-        model = onnx.load("data/mnist-8.onnx")
+        model = onnx.load("/input/tests/data/mnist-8.onnx")
         self.assertIn("CNTKGraph", onnx.helper.printable_graph(model.graph))
