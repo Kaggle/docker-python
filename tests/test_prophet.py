2fce9dc19746964e83c89304744b1c32f111fb0f
Vincent Roseberry <rosbo@google.com>
2022-06-28 00:00:32 +0000

    Fix prophet test
    The new version of prophet (1.1) introduced breaking changes to the fit
    arguments.
    
    http://b/237327883

--- tests/test_prophet.py
+++ tests/test_prophet.py
@@ -8,9 +8,11 @@ from prophet import Prophet
 class TestProphet(unittest.TestCase):
     def test_fit(self):
         train = pd.DataFrame({
-            'ds': np.array(['2012-05-18', '2012-05-20']),
-            'y': np.array([38.23, 21.25])
+            'ds': np.array(['2012-05-18', '2012-05-19', '2012-05-20', '2012-05-21', '2012-05-22']),
+            'y': np.array([38.23, 21.25, 20.18, 20.01, 19.02])
         })
 
         forecaster = Prophet(mcmc_samples=1)
-        forecaster.fit(train, control={'adapt_engaged': False})
+        forecaster.fit(train, adapt_engaged=False)
+
+        self.assertEqual(len(train) + 1, len(forecaster.make_future_dataframe(periods=1)))
