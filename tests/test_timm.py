c568678e223d1d246ad9d79e1f9e8c46978e7c0d
Philmod <philmod@google.com>
2023-01-19 15:52:29 -0500

    change name of test method

--- tests/test_timm.py
+++ tests/test_timm.py
@@ -4,6 +4,6 @@ import timm
 
 
 class TestTimm(unittest.TestCase):
-    def assertList(self):
+    def test_list_models(self):
         models = timm.list_models()
         self.assertGreater(len(models), 0)
