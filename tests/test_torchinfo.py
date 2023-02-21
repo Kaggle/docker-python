540d669920473f4bc48309d2d37b6c40ed37b667
Philmod <philmod@google.com>
2023-02-21 12:01:55 -0500

    Add torchinfo package to summarize a pytorch model

---
+++ tests/test_torchinfo.py
@@ -0,0 +1,11 @@
+import unittest
+
+import torch.nn as tnn
+
+from torchinfo import summary
+
+class TestTorchinfo(unittest.TestCase):
+    def test_info(self):
+        model = linear_torch = tnn.Linear(5,3)
+        s = summary(model)
+        self.assertIn('Layer', s)
