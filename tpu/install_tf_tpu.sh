c185f0d098f7b1f2af1b3f11ce9367a6315086ba
Dustin H <herbison@google.com>
2023-03-01 14:15:46 -0500

    Need force-reinstall so that it actually installs the TPU version

--- tpu/install_tf_tpu.sh
+++ tpu/install_tf_tpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install --no-index --find-links /lib/wheels/sources /lib/wheels/tensorflow-*-cp38-cp38-linux_x86_64.whl
+pip install --force-reinstall --no-index --find-links /lib/wheels/sources /lib/wheels/tensorflow-*-cp38-cp38-linux_x86_64.whl
 cp /lib/patches/kaggle_module_resolver.py /usr/local/lib/python3.8/site-packages/tensorflow_hub/kaggle_module_resolver.py
 sed -i '/from tensorflow_hub import uncompressed_module_resolver/a from tensorflow_hub import kaggle_module_resolver' /usr/local/lib/python3.8/site-packages/tensorflow_hub/config.py
 sed -i '/_install_default_resolvers()/a \ \ registry.resolver.add_implementation(kaggle_module_resolver.KaggleFileResolver())' /usr/local/lib/python3.8/site-packages/tensorflow_hub/config.py
