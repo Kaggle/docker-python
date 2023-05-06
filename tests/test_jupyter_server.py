9e2c3c90e93b099a2715cd330a9e4929ee076583
Maciej Skórski <maciej.skorski@gmail.com>
2023-05-06 17:28:07 +0000

    formatting

--- tests/test_jupyter_server.py
+++ tests/test_jupyter_server.py
@@ -3,14 +3,13 @@ import unittest
 
 
 class TestJupyterServer(unittest.TestCase):
-    
-    
     def test_version(self):
         from packaging.version import parse as parse_version
         from importlib.metadata import version as pckg_version
-        self.assertTrue(parse_version(pckg_version('jupyter_server')) >= parse_version('2.0'))
-        
+
+        self.assertTrue(parse_version(pckg_version("jupyter_server")) >= parse_version("2.0"))
+
     def test_terminals(self):
         import jupyter_server_terminals
 
-    
+        self.assertTrue(hasattr(jupyter_server_terminals, "version_info"))
