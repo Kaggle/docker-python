cf60eda37be580511e9fc4b3ca7a25de40ee9f71
Dhinesh Kumar Devanathan <neshdev@google.com>
2023-08-24 09:14:55 -0400

    Docker image with incremental updates (#1294)
    * Add layer pruning and sync tf-io with tf
    
    * Upgrade to tf=2.13.0
    
    Resolved bq_helper package which was broken as a side effect from the upgrade.
    
    * Removed tensorflow-gcs-config package and test.
    
    Package is being used minimally. See b/296908146#comment9
    
    * Revert tf=2.12
    
    Remove test for `tensorflow_gcs_config`

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,6 +5,5 @@ class TestImport(unittest.TestCase):
     def test_basic(self):
         import bq_helper
         import cleverhans
-        import tensorflow_gcs_config
         import tensorflow_datasets
         import segment_anything
