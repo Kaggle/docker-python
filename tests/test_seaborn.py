180ac9cc6f90b7ffc3529fd3da318fc2938ace88
Philmod <philmod@google.com>
2023-11-01 08:10:19 +0100

    Pin matplotlib as seaborn 0.13.0 is uninstalable because of conflict

--- tests/test_seaborn.py
+++ tests/test_seaborn.py
@@ -7,7 +7,7 @@ import seaborn as sns
 class TestSeaborn(unittest.TestCase):
     # Fails if seaborn gets downgraded by other package installations.
     def test_version(self):
-        self.assertGreaterEqual(StrictVersion(sns.__version__), StrictVersion("0.13.0"))
+        self.assertGreaterEqual(StrictVersion(sns.__version__), StrictVersion("0.12.0"))
 
 
     def test_option(self):
