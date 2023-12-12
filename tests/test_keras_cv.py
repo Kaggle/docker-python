39bfb4b3c7b78c698742c562990b3daf47e5fbd6
Vincent Roseberry <rosbo@google.com>
2023-12-12 04:02:15 +0000

    Update test to use kagglehub fake server

--- tests/test_keras_cv.py
+++ tests/test_keras_cv.py
@@ -4,14 +4,17 @@ import keras_cv
 import keras
 import numpy as np
 
+from utils.kagglehub import create_test_kagglehub_server
+
 class TestKerasCV(unittest.TestCase):
     def test_inference(self):
-        classifier = keras_cv.models.ImageClassifier.from_preset(
-            'efficientnetv2_b0_imagenet_classifier',
-            load_weights=False, # load randomly initialized model from preset architecture with weights
-        )
-        image = keras.utils.load_img('/input/tests/data/face.jpg')
-        image = np.array(image)
-        predictions = classifier.predict(np.expand_dims(image, axis=0))
-        top_classes = predictions[0].argsort(axis=-1)
-        self.assertEqual(1000, len(top_classes))
+        with create_test_kagglehub_server():
+            classifier = keras_cv.models.ImageClassifier.from_preset(
+                'efficientnetv2_b0_imagenet_classifier',
+                load_weights=False, # load randomly initialized model from preset architecture with weights
+            )
+            image = keras.utils.load_img('/input/tests/data/face.jpg')
+            image = np.array(image)
+            predictions = classifier.predict(np.expand_dims(image, axis=0))
+            top_classes = predictions[0].argsort(axis=-1)
+            self.assertEqual(1000, len(top_classes))
--- tests/test_keras_nlp.py
+++ tests/test_keras_nlp.py
@@ -3,26 +3,17 @@ import unittest
 import keras_nlp
 import keras
 import numpy as np
-import tensorflow as tf
+
+from utils.kagglehub import create_test_kagglehub_server
 
 class TestKerasNLP(unittest.TestCase):
     def test_fit(self):
-        # From https://keras.io/api/keras_nlp/models/bert/bert_classifier/
-        features = {
-            "token_ids": tf.ones(shape=(2, 12), dtype=tf.int64),
-            "segment_ids": tf.constant(
-                [[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0]] * 2, shape=(2, 12)
-            ),
-            "padding_mask": tf.constant(
-                [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]] * 2, shape=(2, 12)
-            ),
-        }
-        labels = [0, 3]
-
-        classifier = keras_nlp.models.BertClassifier.from_preset(
-            'bert_tiny_en_uncased',
-            load_weights=False, # load randomly initialized model from preset architecture with weights
-            preprocessor=None, # to avoid downloading the vocabulary
-            num_classes=4,
-        )
-        classifier.fit(x=features, y=labels, batch_size=2)
+        with create_test_kagglehub_server():
+            classifier = keras_nlp.models.BertClassifier.from_preset(
+                'bert_tiny_en_uncased',
+                load_weights=False, # load randomly initialized model from preset architecture with weights
+                num_classes=2,
+                activation="softmax",
+            )
+            result = classifier.predict(['What an amazing movie!'])
+            self.assertEqual((1, 2), result.shape)
