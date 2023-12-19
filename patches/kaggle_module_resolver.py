83789d08987b82561d9ff9c594f2f1a397a8ae34
Vincent Roseberry <rosbo@google.com>
2023-12-19 10:04:00 -0800

    Use kagglehub inside Kaggle Notebook for tensorflow_hub (#1342)
    Currently, if you try using tensorflow_hub with a kaggle.com URL inside
    a Kaggle notebook but the model isn't attached you would get an error
    message telling you to attach the model.
    
    With this change, we are calling `kagglehub` instead and will attach the
    model for the user if it is missing.
    
    http://b/311184735

--- patches/kaggle_module_resolver.py
+++ patches/kaggle_module_resolver.py
@@ -1,12 +1,13 @@
 import os
 import re
+import kagglehub
 
 from tensorflow_hub import resolver
 
-url_pattern = re.compile(r"https?://([a-z]+\.)?kaggle.com/models/[^\\/]+/(?P<model>[^\\/]+)/frameworks/(?P<framework>[^\\/]+)/variations/(?P<variation>[^\\/]+)/versions/(?P<version>[0-9]+)$")
+url_pattern = re.compile(r"https?://([a-z]+\.)?kaggle.com/models/(?P<owner>[^\\/]+)/(?P<model>[^\\/]+)/frameworks/(?P<framework>[^\\/]+)/variations/(?P<variation>[^\\/]+)/versions/(?P<version>[0-9]+)$")
 
 def _is_on_kaggle_notebook():
-    return os.getenv("KAGGLE_CONTAINER_NAME") != None
+    return os.getenv("KAGGLE_KERNEL_RUN_TYPE") != None
 
 def _is_kaggle_handle(handle):
     return url_pattern.match(handle) != None
@@ -17,9 +18,4 @@ class KaggleFileResolver(resolver.HttpResolverBase):
     
     def __call__(self, handle):
         m = url_pattern.match(handle)
-        local_path = f"/kaggle/input/{m.group('model')}/{m.group('framework').lower()}/{m.group('variation')}/{m.group('version')}"
-        if not os.path.exists(local_path):
-            # TODO(b/268256777) Attach model & wait until ready instead.
-            raise RuntimeError(f"You have to attach the '{handle}' model to your Kaggle notebook.")
-        
-        return local_path
+        return kagglehub.model_download(f"{m.group('owner')}/{m.group('model')}/{m.group('framework').lower()}/{m.group('variation')}/{m.group('version')}")
