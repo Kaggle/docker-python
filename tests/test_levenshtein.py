69ad60abb922a59d6688e61f6d51112d341dc28e
Jonathan Calderon Chavez <chavezcalderon@google.com>
2024-04-02 20:54:13 +0000

    package removal pt 2

--- tests/test_levenshtein.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import Levenshtein
-
-class TestLevenshtein(unittest.TestCase):
-    def test_distance(self):
-        distance = Levenshtein.distance('Levenshtein', 'Lenvinsten')
-
-        self.assertEqual(4, distance)
