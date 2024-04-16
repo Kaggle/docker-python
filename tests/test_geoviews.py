e91af4d2316675999410eca13c289435aa79a7d6
Johnny Chavez <64660690+calderjo@users.noreply.github.com>
2024-04-15 17:42:53 -0700

    Update TestGeoviews (#1383)
    looks like Geoviews now requires loading a backend plotting extension
    
    updated test to do this.
    
    tested locally
    
    cc: @jplotts

--- tests/test_geoviews.py
+++ tests/test_geoviews.py
@@ -1,11 +1,12 @@
 import unittest
 
 import geoviews.feature as gf
-
+import holoviews as hv
 from cartopy import crs
 
 class TestGeoviews(unittest.TestCase):
     def test_viz(self):
+        hv.extension('matplotlib')
         (gf.ocean + gf.land + gf.ocean * gf.land * gf.coastline * gf.borders).options(
             'Feature', projection=crs.Geostationary(), global_extent=True
         ).cols(3)
