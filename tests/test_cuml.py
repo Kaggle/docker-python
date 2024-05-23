190008f38d02e41f3b078136ba4eda2b996ac4a9
Jonathan Calderon Chavez <chavezcalderon@google.com>
2024-05-23 17:36:11 +0000

    add test

---
+++ tests/test_cuml.py
@@ -0,0 +1,19 @@
+import unittest
+
+from common import gpu_test, p100_exempt
+
+
+class TestCumlPCA(unittest.TestCase):
+    @gpu_test
+    @p100_exempt
+    def test_pca_fit_transform(self):
+        import unittest
+        import numpy as np
+        from cuml.decomposition import PCA
+
+        x = np.array([[1.0, 2.0], [2.0, 4.0], [3.0, 6.0], [-1.0, -2.0], [-2.0, -4.0]])  
+        pca = PCA(n_components=1)
+
+        x_transformed = pca.fit_transform(x)
+
+        self.assertEqual(x_transformed.shape, (5, 1)) 
