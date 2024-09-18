b48d1bf834181917587c24c9a37dfc9c2672ddb1
Jonathan Calderon Chavez <chavezcalderon@google.com>
2024-09-18 17:41:24 +0000

    sjdlfksjdlfkjsdfkj

--- tests/test_matplotlib.py
+++ tests/test_matplotlib.py
@@ -1,10 +1,16 @@
 import unittest
 import os.path
 
+from distutils.version import StrictVersion
+
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
 class TestMatplotlib(unittest.TestCase):
+    def test_version(self):
+        self.assertLess(StrictVersion(matplotlib.__version__), StrictVersion("3.8.0"))
+
     def test_plot(self):
         plt.plot(np.linspace(0,1,50), np.random.rand(50))
         plt.savefig("plot1.png")
