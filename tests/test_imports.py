8303bd88ad61f83f0ed94723166164ab271ff5f1
Johnny Chavez <64660690+calderjo@users.noreply.github.com>
2024-09-19 09:40:29 -0700

    Mixed bag clean up (#1425)
    yeah handful of things
    
    1) Combined some layer
    
    2) made comments referring to bugs use consistent format 
    
    3) removed low usage packages and tests, we recently launched package
    manager. we can confidently remove more unused packages. The following
    packages are to be removed:
    - pykalman
    - preprocessing
    - hmmlearn
    - gplearn
    - geoplot
    - polyglot
    - ggplot
    - descartes
    - fitter
    - imagecodecs
    - wfdb
    - hpsklearn
    - cleverhans
    - osmnx
    - pysal
    - wordsegment
    - vowpalwabbit
    - feather
    - kmodes
    - ortools
    - mlens
    - vecstack
    - Geohash
    - geoviews
    - s2sphere
    - flashtext
    - kmapper
    - stemming
    - hunspell
    - spectral
    - essentia
    - hypertools
    - stop_words
    - scattertext
    - vaex
    - blake3
    - catalyst (note no longer maintained since 2022)

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -4,6 +4,5 @@ class TestImport(unittest.TestCase):
     # Basic import tests for packages without any.
     def test_basic(self):
         import bq_helper
-        import cleverhans
         import tensorflow_datasets
         import segment_anything
