a762dbe4e2de06c3b3951016b90473dfc8f99201
DJ Sterling <dster@google.com>
2024-09-27 12:05:50 -0600

    add test to polars test

--- tests/test_polars.py
+++ tests/test_polars.py
@@ -2,9 +2,14 @@ import unittest
 
 import polars as pl
 
-class TestPolars(unittest.TestCase):    
+class TestPolars(unittest.TestCase):
     def test_read_csv(self):
-        data = pl.read_csv("/input/tests/data/train.csv")
+        data = pl.read_csv('/input/tests/data/train.csv')
 
         self.assertEqual(100, len(data))
+
+    def test_plot(self):
+        # This relies on the hvplot package
+        data = pl.read_csv('/input/tests/data/train.csv')
+        data.plot.line()
 
