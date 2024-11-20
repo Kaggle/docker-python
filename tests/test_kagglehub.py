59ff1810ebc5b83b639a44274c69bff92a8a87ea
Jonathan Calderon Chavez <chavezcalderon@google.com>
2024-11-20 05:01:27 +0000

    d

--- tests/test_kagglehub.py
+++ tests/test_kagglehub.py
@@ -8,8 +8,10 @@ class TestKagglehub(unittest.TestCase):
     def test_login(self):
         with self.assertLogs('kagglehub', level='INFO') as l:
             with mock.patch("builtins.input") as mock_input:
-                mock_input.side_effect = ["lastplacelarry", "some-key"]
-                # Disabling credentials validation since network access is disabled in unittest.
-                kagglehub.login(validate_credentials=False)
+                with mock.patch("getpass.getpass") as mock_getpass:
+                    mock_input.side_effect = ["lastplacelarry"]
+                    mock_getpass.return_value = "some-key"
 
-                self.assertIn("credentials set", l.output[0])
+                    kagglehub.login(validate_credentials=False)
+
+                    self.assertIn("credentials set", l.output[0])
