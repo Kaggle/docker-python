d9160941c247aee02f7ae5554f3f086b9f613d5a
Dustin Herbison <herbison@google.com>
2024-11-27 21:04:02 +0000

    disable cuML test since its not on colab

--- tests/test_cuml.py
+++ tests/test_cuml.py
@@ -6,6 +6,7 @@ from common import gpu_test, p100_exempt
 class TestCuml(unittest.TestCase):
     @gpu_test
     @p100_exempt # b/342143152: cuML(>=24.4v) is inompatible with p100 GPUs.
+    @unittest.skip("cuML is not installed in Colab.")
     def test_pca_fit_transform(self):
         import unittest
         import numpy as np
