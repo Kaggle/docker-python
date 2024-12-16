ad7d1a79a3acc7ac1344f0bbfe1fae9faa686f67
Prathamesh Bang <psbang@google.com>
2024-12-16 20:20:16 +0000

    Typo fix

--- tests/test_torchtune.py
+++ tests/test_torchtune.py
@@ -2,7 +2,7 @@ import unittest
 
 import subprocess
 
-class TestAlbumentations(unittest.TestCase):
+class TestTorchtune(unittest.TestCase):
     def test_help(self):
         ret_code = subprocess.run(["tune", "--help"])
         self.assertEqual(0, ret_code.returncode)
