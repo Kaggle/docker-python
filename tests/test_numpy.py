083bc20f00eda74a422ab91b9c18de7a80806d07
Dustin H <herbison@google.com>
2025-01-03 14:54:47 -0500

    Update Colab base image to release-colab_20241217-060132_RC00 (#1458)
    http://b/385145217

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -3,9 +3,10 @@ import unittest
 from distutils.version import StrictVersion
 
 import numpy as np
-from numpy.distutils.system_info import get_info
+import io
+from contextlib import redirect_stdout
 
-class TestNumpy(unittest.TestCase):   
+class TestNumpy(unittest.TestCase):
     def test_version(self):
         # b/370860329: newer versions are not capable with current tensorflow
         self.assertEqual(StrictVersion(np.__version__), StrictVersion("1.26.4")) 
@@ -18,5 +19,13 @@ class TestNumpy(unittest.TestCase):
     # Numpy must be linked to the MKL. (Occasionally, a third-party package will muck up the installation
     # and numpy will be reinstalled with an OpenBLAS backing.)
     def test_mkl(self):
-        # This will throw an exception if the MKL is not linked correctly or return an empty dict.
-        self.assertTrue(get_info("blas_mkl"))
+        try:
+            from numpy.distutils.system_info import get_info
+            # This will throw an exception if the MKL is not linked correctly or return an empty dict.
+            self.assertTrue(get_info("blas_mkl"))
+        except:
+            # Fallback to check if mkl is present via show_config()
+            config_out = io.StringIO()
+            with redirect_stdout(config_out):
+                np.show_config()
+            self.assertIn("mkl_rt", config_out.getvalue())
