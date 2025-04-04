77057031413856b3160860b637106a33f98c9fa4
Jonathan Calderon Chavez <chavezcalderon@google.com>
2025-04-04 05:01:54 +0000

    model's bug fix

--- tests/test_torchtune.py
+++ tests/test_torchtune.py
@@ -1,9 +1,10 @@
 import unittest
-
 import subprocess
 
 class TestTorchtune(unittest.TestCase):
     def test_help(self):
-        ret_code = subprocess.run(["tune", "--help"])
-        self.assertEqual(0, ret_code.returncode)
-        self.assertIsNone(ret_code.stderr)
+        result = subprocess.run(["tune", "--help"], stdout=subprocess.PIPE)
+
+        self.assertEqual(0, result.returncode)
+        self.assertIsNone(result.stderr)
+        self.assertIn("Download a model from the Hugging Face Hub or Kaggle Model Hub.", result.stdout.decode("utf-8"))
