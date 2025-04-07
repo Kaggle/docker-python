c1dd2e3f78192808ed0f1252c96aa8204323c441
Jonathan Calderon Chavez <chavezcalderon@google.com>
2025-04-07 20:55:05 +0000

    make sure transformer in up to date

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -1,7 +1,7 @@
 import unittest
 
 import torch
-from transformers import AdamW
+import torch.optim as optim
 import transformers.pipelines # verify this import works
 
 
@@ -10,13 +10,12 @@ class TestTransformers(unittest.TestCase):
         self.assertEqual(len(list1), len(list2))
         for a, b in zip(list1, list2):
             self.assertAlmostEqual(a, b, delta=tol)
-
     def test_adam_w(self):
         w = torch.tensor([0.1, -0.2, -0.1], requires_grad=True)
         target = torch.tensor([0.4, 0.2, -0.5])
         criterion = torch.nn.MSELoss()
         # No warmup, constant schedule, no gradient clipping
-        optimizer = AdamW(params=[w], lr=2e-1, weight_decay=0.0)
+        optimizer = optim.AdamW(params=[w], lr=2e-1, weight_decay=0.0)
         for _ in range(100):
             loss = criterion(w, target)
             loss.backward()
