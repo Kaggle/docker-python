20b586a569612418bb1e3c3717152a28476ee933
mariogemoll <mariogemoll@gmx.net>
2025-06-18 15:03:38 +0200

    Add anywidget

---
+++ tests/test_anywidget.py
@@ -0,0 +1,20 @@
+import unittest
+import subprocess
+import re
+
+
+# Remove ANSI coloring escape codes
+def strip_ansi(text):
+    ansi_escape = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
+    return ansi_escape.sub("", text)
+
+
+class TestAnyWidget(unittest.TestCase):
+
+    def test_labextension(self):
+        output = subprocess.check_output(
+            ["jupyter", "labextension", "list"], stderr=subprocess.STDOUT, text=True
+        )
+        output = strip_ansi(output)
+        match = re.search(r"^\s*anywidget\s+v[\d.]+\s+enabled.*$", output, re.MULTILINE)
+        self.assertIsNotNone(match, "anywidget not found in labextension list")
