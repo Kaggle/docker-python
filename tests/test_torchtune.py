f6db354afe8a5395a98f35113814f0eaaa7c3fbe
Johnny Chavez <64660690+calderjo@users.noreply.github.com>
2025-06-27 00:45:37 +0000

    Fix torch tune, keras, tensorflow tests (#1489)
    Looks like torch tune changed the output of the --help command, this
    cause issues with our smoke tests.
    Keras, along with other package had issues with existing issues with
    cudnn downgrading due to torch requirements, we pinned relevant tests.

--- tests/test_torchtune.py
+++ tests/test_torchtune.py
@@ -3,8 +3,14 @@ import subprocess
 
 class TestTorchtune(unittest.TestCase):
     def test_help(self):
-        result = subprocess.run(["tune", "--help"], stdout=subprocess.PIPE)
+        result = subprocess.run(
+            ["tune", "--help"], 
+            capture_output=True,
+            text=True
+        )
 
         self.assertEqual(0, result.returncode)
-        self.assertIsNone(result.stderr)
-        self.assertIn("Download a model from the Hugging Face Hub or Kaggle Model Hub.", result.stdout.decode("utf-8"))
+        self.assertIn(
+            "Download a model from the Hugging Face Hub or Kaggle", 
+            result.stdout
+        )
