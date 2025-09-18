d019ef74b14fa4281074e1e8709f077e8fbc78c7
Eric Johnson <65414824+metrizable@users.noreply.github.com>
2025-09-18 13:24:17 -0700

    Include the default PyPI for missing libucx-cu12 package version. (#1495)
    The CPU/GPU container image builds are currently broken due to
    dependency resolution failures:
    
    <img width="856" height="242" alt="9MgPMDp9qN8qCqf"
    src="https://github.com/user-attachments/assets/b30c6024-0391-4c54-a441-502847673d7c"
    />
    
    It appears, this version was removed from the Nvidia index since the
    last build.
    
    <img width="1099" height="281" alt="6NFm9uDQDA5efcV"
    src="https://github.com/user-attachments/assets/c4756d56-4f21-43fc-8591-3eccfcb74dc0"
    />
    
    We ensure that a compatible package version `libucx-cu12==1.18.1` is
    available by including the default PyPI and specifying an appropriate
    [index
    strategy](https://docs.astral.sh/uv/concepts/indexes/#searching-across-multiple-indexes)
    to `uv`.
    
    In the near future, we may want to consider upgrading `cuml-cu12` and
    ecosystem to 25.6 or later.

--- tests/test_fastai.py
+++ tests/test_fastai.py
@@ -1,35 +1,36 @@
 import unittest
 
 import fastai
-
 from fastai.tabular.all import *
 
+
 class TestFastAI(unittest.TestCase):
-    # Basic import
-    def test_basic(self):
-        import fastai
-        import fastcore
-        import fastprogress
-        import fastdownload
+  # Basic import
+  def test_basic(self):
+    import fastai
+    import fastcore
+    import fastprogress
+    import fastdownload
 
-    def test_has_version(self):
-        self.assertGreater(len(fastai.__version__), 2)
-    
-    # based on https://github.com/fastai/fastai/blob/master/tests/test_torch_core.py#L17
-    def test_torch_tensor(self):
-        a = tensor([1, 2, 3])
-        b = torch.tensor([1, 2, 3])
+  def test_has_version(self):
+    self.assertGreater(len(fastai.__version__), 2)
 
-        self.assertTrue(torch.all(a == b))
+  # based on https://github.com/fastai/fastai/blob/master/tests/test_torch_core.py#L17
+  def test_torch_tensor(self):
+    a = tensor([1, 2, 3])
+    b = torch.tensor([1, 2, 3])
 
-    def test_tabular(self):
-        dls = TabularDataLoaders.from_csv(
-            "/input/tests/data/train.csv",
-            cont_names=["pixel"+str(i) for i in range(784)],
-            y_names='label',
-            procs=[FillMissing, Categorify, Normalize])
-        learn = tabular_learner(dls, layers=[200, 100])
-        with learn.no_bar():
-            learn.fit_one_cycle(n_epoch=1)
-        
-            self.assertGreater(learn.smooth_loss, 0)
+    self.assertTrue(torch.all(a == b))
+
+  def test_tabular(self):
+    dls = TabularDataLoaders.from_csv(
+        "/input/tests/data/train.csv",
+        cont_names=["pixel" + str(i) for i in range(784)],
+        y_names="label",
+        procs=[FillMissing, Categorify, Normalize],
+    )
+    learn = tabular_learner(dls, layers=[200, 100])
+    with learn.no_bar():
+      learn.fit_one_cycle(n_epoch=1)
+
+      self.assertGreater(learn.smooth_loss, 0)
