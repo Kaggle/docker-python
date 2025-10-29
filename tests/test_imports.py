0cb222723ba71fa79c9cf0694297586f53840f49
Johnny Chavez <64660690+calderjo@users.noreply.github.com>
2025-10-29 16:14:48 -0700

    Remove even more references to AutoMl and Bq (#1511)

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,6 +3,5 @@ import unittest
 class TestImport(unittest.TestCase):
     # Basic import tests for packages without any.
     def test_basic(self):
-        import bq_helper
         import tensorflow_datasets
         import segment_anything
