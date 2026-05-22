e30b005b7ba3fd412754ffb8762c391d86b2d55a
Vincent Roseberry <rosbo@google.com>
2026-05-22 21:41:53 +0000

    Fix kornia test failure
    kornia.utils.image_to_tensor is deprecated and not lazy loaded
    anymore. Using the new location instead.
    
    http://b/515825952

--- tests/test_kornia.py
+++ tests/test_kornia.py
@@ -7,7 +7,7 @@ import kornia
 class TestKornia(unittest.TestCase):
     def test_imread_opencv(self):
         img = cv2.imread('/input/tests/data/dot.png')
-        img_t = kornia.utils.image_to_tensor(img)
+        img_t = kornia.image.image_to_tensor(img)
 
         self.assertEqual(img.shape, (1, 1, 3))
         self.assertEqual(img_t.shape, (3, 1, 1))
